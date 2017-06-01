6201202636627508d46a72b7fb7536b1e367b490
Ruby Lim <ruby.b.lim@gmail.com>
2017-06-01 12:32:43 -0700

    Assignment 23 Rails: Auth User Model completed

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ActiveRecord::Base
   before_save { self.email = email.downcase if email.present? }
+  before_save :format_name
+
 
   validates :name, length: { minimum: 1, maximum: 100 }, presence: true
   validates :password, presence: true, length: { minimum: 6 }, if: "password_digest.nil?"
@@ -9,4 +11,14 @@ class User < ActiveRecord::Base
             uniqueness: { case_sensitive: false },
             length: { minimum: 3, maximum: 254 }
   has_secure_password
+
+  def format_name
+    if name
+      array = []
+      name.split.each do |n|
+        array << n.capitalize
+      end
+      self.name = array.join(" ")
+    end
+  end
 end
