ec349a73dac2e43067faf54f1f1ea31fb9499848
Ruby Lim <ruby.b.lim@gmail.com>
2017-06-04 16:14:17 -0700

    Assignment 24 Rails: Auth Signing Up completed

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,9 @@ Rails.application.routes.draw do
 
   resources :users, only: [:new, :create]
 
+
   get 'about' => 'welcome#about'
+  post 'users/confirm' => 'users#confirm'
 
   root 'welcome#index'
 end
