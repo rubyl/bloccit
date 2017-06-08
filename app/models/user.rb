0bbc4d8fde949e275c3cbfb9be389e1d2e2c115a
Ruby Lim <ruby.b.lim@gmail.com>
2017-06-07 23:42:34 -0700

    Checkpoint 27 Rails: Posts and Users completed

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ActiveRecord::Base
   before_save { self.email = email.downcase if email.present? }
+  has_many :posts
 
   validates :name, length: { minimum: 1, maximum: 100 }, presence: true
   validates :password, presence: true, length: { minimum: 6 }, if: "password_digest.nil?"
