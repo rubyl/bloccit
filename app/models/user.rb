ea8f0759e291efee92b7fec5160eb6af9e9cecc5
Ruby Lim <ruby.b.lim@gmail.com>
2017-06-09 17:17:55 -0700

    Checkpoint 28 Rails: Authorization completed

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ActiveRecord::Base
   before_save { self.email = email.downcase if email.present? }
+  before_save { self.role ||= :member }
   has_many :posts
 
   validates :name, length: { minimum: 1, maximum: 100 }, presence: true
@@ -10,4 +11,6 @@ class User < ActiveRecord::Base
             uniqueness: { case_sensitive: false },
             length: { minimum: 3, maximum: 254 }
   has_secure_password
+
+  enum role: [:member, :admin]
 end
