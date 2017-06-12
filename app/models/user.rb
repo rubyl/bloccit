206c18c56a5736a88ed5d59e198ae694a82c2d5c
Ruby Lim <ruby.b.lim@gmail.com>
2017-06-11 17:32:54 -0700

    Assignment 28 Rails: Authorization completed

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,5 +12,5 @@ class User < ActiveRecord::Base
             length: { minimum: 3, maximum: 254 }
   has_secure_password
 
-  enum role: [:member, :admin]
+  enum role: [:member, :admin, :moderator]
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -36,6 +36,10 @@ RSpec.describe User, type: :model do
     it "responds to role" do
       expect(user).to respond_to(:member?)
     end
+
+    it "responds to role" do
+      expect(user).to respond_to(:moderator?)
+    end
   end
 
   describe "roles" do
@@ -51,22 +55,48 @@ RSpec.describe User, type: :model do
       it "returns false for #admin?" do
         expect(user.admin?).to be_falsey
       end
+
+      it "returns false for #admin?" do
+        expect(user.moderator?).to be_falsey
+      end
     end
 
     context "admin user" do
       before do
         user.admin!
+      end
+
+      it "returns false for #member?" do
+        expect(user.member?).to be_falsey
+      end
+
+      it "returns false for #moderator?" do
+        expect(user.member?).to be_falsey
+      end
+
+      it "returns true for #admin?" do
+        expect(user.admin?).to be_truthy
+      end
     end
 
-    it "returns false for #member?" do
-      expect(user.member?).to be_falsey
-    end
+    context "admin user" do
+      before do
+        user.moderator!
+      end
 
-    it "returns true for #admin?" do
-      expect(user.admin?).to be_truthy
+      it "returns false for #member?" do
+        expect(user.member?).to be_falsey
+      end
+
+      it "returns false for #admin?" do
+        expect(user.admin?).to be_falsey
+      end
+
+      it "returns true for #moderator?" do
+        expect(user.moderator?).to be_truthy
+      end
     end
   end
-end
 
   describe "invalid user" do
     let(:user_with_invalid_name) { User.new(name: "", email: "user@bloccit.com") }
